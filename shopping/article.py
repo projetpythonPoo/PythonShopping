0a6c3301e40686117f536ce0ca66380f5611e3b3
leo-jaquier <leo.jaquier@cpnv.ch>
2023-11-10 11:01:26 +0100

    fix tets first test successful

--- shopping/article.py
+++ shopping/article.py
@@ -1,18 +1,21 @@
 
 class Article:
-    def __init__(self, id_=int(0), description=str(""), price=float(0.0)):
+    def __init__(self, id_, description, price):
         self._id = id_
         self._description = description
         self._price = price
 
+    @property
     def get_id(self):
         return self._id
 
+    @property
     def get_description(self):
         return self._description
 
     def set_description(self, value):
         self._description = value
 
+    @property
     def get_price(self):
         return self._price
--- tests/testArticle.py
+++ tests/testArticle.py
@@ -3,6 +3,7 @@ from shopping.article import Article
 
 
 class TestArticle(unittest.TestCase):
+
     def setUp(self):
         # Private attributes
         self._article = None
@@ -10,12 +11,12 @@ class TestArticle(unittest.TestCase):
         self._description = ""
         self._price = 0.0
 
+    def test_all_properties_after_instantiation_success(self):
+        # given
         self._id = 1
         self._description = "product description"
         self._price = 20.45
-        self._article = Article(self._id, self._description, self._price)
-
-    def test_all_properties_after_instantiation_success(self):
+        self._article = Article(self._id,self._description,self._price)
         # then
         self.assertEqual(self._id, self._article.get_id)
         self.assertEqual(self._description, self._article.get_description)
